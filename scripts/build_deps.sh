5f624c31e106258e442b623e118ee70181ad3421
albert.shi <heng.shi@enflame-tech.com>
2021-04-20 15:21:27 +0800

    add llvm mlir build scripts and submodule

---
+++ scripts/build_deps.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+script_path=`dirname $0`
+script_realpath=`realpath $script_path`
+echo "script path = "$script_realpath
+top_dir_path=$script_path"/.."
+top_dir_realpath=`realpath $top_dir_path`
+echo "top dir = "$top_dir_realpath
+
+# create mlir build path
+mkdir ${top_dir_realpath}/mlir_build
+cd ${top_dir_realpath}/mlir_build
+mkdir -p ${top_dir_realpath}/mlir_build/install_dir
+
+# config mlir
+# build with clang
+cmake -G Ninja ${top_dir_realpath}/external/llvm-project/llvm -DLLVM_ENABLE_PROJECTS=mlir -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=DEBUG -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_LLD=ON -DCMAKE_C_COMPILER=clang-11 -DCMAKE_CXX_COMPILER=clang++-11 -DCMAKE_INSTALL_PREFIX=${top_dir_realpath}/mlir_build/install_dir -DLLVM_INSTALL_UTILS=ON
+# build with gcc
+# cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS=mlir -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=./install_dir
+
+# build mlir
+cmake --build . --target check-mlir
+cmake --build . --target install
+
+cd -
