7fa7a69d26c5e03bed180e6e0d01a1a75f94e33c
albert.shi <heng.shi@enflame-tech.com>
2021-04-20 16:12:54 +0800

    setting up mlir basic template

--- scripts/build_cancer.sh
+++ scripts/build_cancer.sh
@@ -7,6 +7,10 @@ top_dir_path=$script_path"/.."
 top_dir_realpath=`realpath $top_dir_path`
 echo "top dir = "$top_dir_realpath
 
+# create mlir build path
+mkdir ${top_dir_realpath}/build
+cd ${top_dir_realpath}/build
+
 cmake -G Ninja .. -DMLIR_DIR=${top_dir_realpath}/mlir_build/install_dir/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=${top_dir_realpath}/mlir_build/bin/llvm-lit
 
 cmake --build . --target standalone-opt
