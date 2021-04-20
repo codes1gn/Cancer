9947a23b4d0e7b20528cc036965cfc8ea0c1ffdb
albert.shi <heng.shi@enflame-tech.com>
2021-04-20 16:05:17 +0800

    add README

--- scripts/build_cancer.sh
+++ scripts/build_cancer.sh
@@ -1,3 +1,12 @@
-cmake -G Ninja .. -DMLIR_DIR=/root/mlir_tutorials/llvm-project/build/install_dir/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=/root/mlir_tutorials/llvm-project/build/bin/llvm-lit
+#!/bin/sh
+
+script_path=`dirname $0`
+script_realpath=`realpath $script_path`
+echo "script path = "$script_realpath
+top_dir_path=$script_path"/.."
+top_dir_realpath=`realpath $top_dir_path`
+echo "top dir = "$top_dir_realpath
+
+cmake -G Ninja .. -DMLIR_DIR=${top_dir_realpath}/mlir_build/install_dir/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=${top_dir_realpath}/mlir_build/bin/llvm-lit
 
 cmake --build . --target standalone-opt
