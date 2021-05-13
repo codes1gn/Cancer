9b2dc56d3d09e03bd2ffde8ff392db2a3060a2e9
albert.shi <heng.shi@enflame-tech.com>
2021-05-13 17:28:43 +0800

    add python envs detect in cmakelist

---
+++ cmake_modules/EnvFuncs.cmake
@@ -0,0 +1,19 @@
+function(cancer_detect_pybind11_install)
+  if(pybind11_DIR)
+    message(STATUS "Using explicit pybind11 cmake directory: ${pybind11_DIR} (-Dpybind11_DIR to change)")
+  else()
+    message(STATUS "Checking for pybind11 in python path...")
+    execute_process(
+      COMMAND "${Python_EXECUTABLE}"
+      -c "import pybind11;print(pybind11.get_cmake_dir(), end='')"
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      RESULT_VARIABLE STATUS
+      OUTPUT_VARIABLE PACKAGE_DIR
+      ERROR_QUIET)
+    if(NOT STATUS EQUAL "0")
+      message(CHECK_FAIL "not found (install via 'pip install pybind11' or set pybind11_DIR)")
+      return()
+    endif()
+    set(pybind11_DIR "${PACKAGE_DIR}" PARENT_SCOPE)
+  endif()
+endfunction()
