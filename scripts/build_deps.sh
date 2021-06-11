2fd8a44c2c052e04697427577cd95aaf75762861
albert.shi <heng.shi@enflame-tech.com>
2021-06-11 16:32:49 +0800

    fix mlir/llvm linking to libMLIR.so issue, option LLVM_ENABLE_LLD=ON failure, use -fuse-ld=ld.lld instead. But we may use LLVM_USE_LINKER=ld.lld. in future

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -12,9 +12,25 @@ mkdir ${top_dir_realpath}/mlir_build
 cd ${top_dir_realpath}/mlir_build
 mkdir -p ${top_dir_realpath}/mlir_build/install_dir
 
+# try to fix linker issue
+export LDFLAGS=-fuse-ld=$(which ld.lld)
+
 # config mlir
 # build with clang
-cmake -G Ninja ${top_dir_realpath}/external/llvm-project/llvm -DLLVM_ENABLE_PROJECTS=mlir -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=DEBUG -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_LLD=ON -DCMAKE_C_COMPILER=clang-11 -DCMAKE_CXX_COMPILER=clang++-11 -DCMAKE_INSTALL_PREFIX=${top_dir_realpath}/mlir_build/install_dir -DLLVM_INSTALL_UTILS=ON
+cmake -G Ninja \
+    ${top_dir_realpath}/external/llvm-project/llvm \
+    -DLLVM_ENABLE_PROJECTS=mlir \
+    -DLLVM_BUILD_EXAMPLES=ON \
+    -DLLVM_TARGETS_TO_BUILD="X86" \
+    -DCMAKE_BUILD_TYPE=DEBUG \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_C_COMPILER=clang-11 \
+    -DCMAKE_CXX_COMPILER=clang++-11 \
+    -DCMAKE_INSTALL_PREFIX=${top_dir_realpath}/mlir_build/install_dir \
+    -DLLVM_INSTALL_UTILS=ON \
+    -DLLVM_BUILD_LLVM_DYLIB=ON \
+    -DLLVM_LINK_LLVM_DYLIB=ON
+    # -DLLVM_ENABLE_LLD=ON \
 # build with gcc
 # cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS=mlir -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=./install_dir
 
