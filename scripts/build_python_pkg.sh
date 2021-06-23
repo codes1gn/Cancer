0a6be5a03f3513d6057e17f8b69e773f88b4f648
albert.shi <heng.shi@enflame-tech.com>
2021-06-23 17:30:22 +0800

    experiment: pybind

---
+++ scripts/build_python_pkg.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+
+script_path=`dirname $0`
+script_realpath=`realpath $script_path`
+echo "script path = "$script_realpath
+top_dir_path=$script_path"/.."
+top_dir_realpath=`realpath $top_dir_path`
+echo "top dir = "$top_dir_realpath
+
+# create mlir build path
+cd ${top_dir_realpath}
+
+# invoke setup.py to build wheel installation pkg
+python setup.py bdist_wheel
+
