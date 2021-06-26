afd077cbd9b0617fcdcb14ac230f3c76d81d853e
albert.shi <heng.shi@enflame-tech.com>
2021-06-26 23:25:56 +0800

    fix so loader for setup.py install

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import subprocess
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
-CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # A CMakeExtension needs a sourcedir instead of a file list.
 # The name must be the _single_ output extension from the CMake build.
@@ -22,41 +21,47 @@ class CMakeBuild(build_ext):
         # CMake lets you override the generator - we need to check this.
         cmake_generator = "-GNinja"
 
+        build_dir = os.path.abspath(
+          os.path.dirname(
+            self.get_ext_fullpath(ext.name)
+          )
+        )
+        if not build_dir.endswith(os.path.sep):
+            build_dir += os.path.sep
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        if not os.path.exists(build_dir):
+            os.makedirs(build_dir)
+
         # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
         # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
         # from Python.
         cmake_args = [
             "-DMLIR_DIR={}".format("/root/research/cancer/mlir_build/install_dir/lib/cmake/mlir"),
+            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
             "-DLLVM_EXTERNAL_LIT={}".format("/root/research/cancer/mlir_build/bin/llvm-lit"),
             "-DCMAKE_BUILD_TYPE={}".format("DEBUG"),  # not used on MSVC, but no harm
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(build_dir),
+            "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
         ]
         build_args = []
         build_args += ["-j8"]
         build_args += ["--verbose"]
-        build_args += ["--clean-first"]
+        # build_args += ["--clean-first"]
         build_args += ["--target", "cancer_compiler"]
 
-        self.src_dir = "../.."
-
-        self.build_dir = os.path.abspath(
-          os.path.dirname(
-            self.get_ext_fullpath(ext.name)
-          )
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
         )
-        if not self.build_dir.endswith(os.path.sep):
-            self.build_dir += os.path.sep
-
-        if not os.path.exists(self.build_dir):
-            os.makedirs(self.build_dir)
-
 
         subprocess.check_call(
-            ["cmake", cmake_generator, self.src_dir] + cmake_args, cwd=self.build_dir
-                )
-
-        subprocess.check_call(
-            ["cmake", "--build", "."] + build_args, cwd=self.build_dir
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
         )
+
+        subprocess.check_call(["cp", "./cancer/CompilerBackend/cancer_compiler.cpython-36m-x86_64-linux-gnu.so", build_dir], cwd=self.build_temp)
+
 
 
 # The information here can also be placed in setup.cfg - better separation of
