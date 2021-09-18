a42c9472a97388ffaf4f7294aa77fd8a4363e7ff
CodeS1gn <codefisheng@gmail.com>
2021-09-18 06:54:11 +0000

    setting up python -> mlir end2end tests

--- cancer_frontend/python/python_jit_runner.py
+++ cancer_frontend/python/python_jit_runner.py
@@ -16,6 +16,7 @@ from cancer_frontend.scaffold.mlir_dialects import *
 from cancer_frontend.scaffold.utils import *
 
 MlirNode = astnodes.Node
+MlirSsaId = astnodes.SsaId
 import types
 
 
@@ -91,7 +92,7 @@ class PythonRunner:
         dump_str += "*******************&&&&&"
         dump_str += ColorPalette.FAIL
         dump_str += "\ndumping mlir ast\n"
-        dump_str += _ast.pretty()
+        # dump_str += _ast.pretty()
         dump_str += ColorPalette.ENDC
         dump_str += ColorPalette.HEADER
         dump_str += "\ndumping mlir IR\n"
@@ -106,7 +107,7 @@ class PythonRunner:
         dump_str += ColorPalette.FAIL
         dump_str += "\ndumping python ast\n"
         # TODO use astunparse as alternative to ast pretty dump. this is not supported before py 3.8
-        dump_str += astunparse.dump(_ast)
+        # dump_str += astunparse.dump(_ast)
         dump_str += ColorPalette.ENDC
         dump_str += ColorPalette.HEADER
         dump_str += "\ndumping python code\n"
@@ -134,3 +135,68 @@ class PythonRunner:
         pyast = ast.parse(code, filename=code_file)
         ast.increment_lineno(pyast, n=start_lineno - 1)
         return pyast
+
+    # TODO may change into classmethod or staticmethod
+    def convert_python_to_mlir(self, pyast: ast.AST) -> MlirNode:
+        from mlir.dialects.standard import ReturnOperation
+        from cancer_frontend.scaffold.mlir_dialects.dialect_tcf import TCF_AddOp
+
+        # this is return op
+        #############################################################################
+        # TODO understand what does match means
+        _returnop = ReturnOperation(match=0)
+        _ssaid_return = [MlirSsaId(value="0", op_no=None)]
+        _returnop.values = _ssaid_return
+
+        _rtype = [astnodes.FloatType(type=astnodes.FloatTypeEnum.f32)]
+        _returnop.types = _rtype
+
+        _returnop_wrapper = astnodes.Operation(result_list=None, op=_returnop, location=None)
+        #############################################################################
+
+        # this is tcf.add op
+        #############################################################################
+        _addop_result = [astnodes.OpResult(value=_ssaid_return[0], count=None)]
+        _ssaid = [MlirSsaId(value="arg0", op_no=None)]
+        _rtype = [
+            astnodes.FunctionType(
+                argument_types=[
+                    astnodes.FloatType(type=astnodes.FloatTypeEnum.f32),
+                    astnodes.FloatType(type=astnodes.FloatTypeEnum.f32),
+                ],
+                result_types=[astnodes.FloatType(type=astnodes.FloatTypeEnum.f32)],
+            )
+        ]
+        _addop1 = TCF_AddOp(match=0, operand_a=_ssaid, operand_b=_ssaid, dtype=_rtype)
+        _addop_wrapper = astnodes.Operation(result_list=_addop_result, op=_addop1, location=None)
+        #############################################################################
+
+        # this is block and region
+        #############################################################################
+        _block = astnodes.Block(label=None, body=[_addop_wrapper, _returnop_wrapper])
+        _region = astnodes.Region(body=[_block])
+        #############################################################################
+
+        # this is function
+        #############################################################################
+        _name = astnodes.SymbolRefId(value="scalar_add_float")
+        _args = [astnodes.NamedArgument(name=_ssaid, type=astnodes.FloatType(type=astnodes.FloatTypeEnum.f32))]
+        _function = astnodes.Function(
+            name=_name,
+            args=_args,
+            result_types=astnodes.FloatType(type=astnodes.FloatTypeEnum.f32),
+            region=_region,
+            attributes=None,
+        )
+        _function_wrapper = astnodes.Operation(result_list=[], op=_function, location=None)
+        #############################################################################
+
+        # this is outter module, out_block and out_region
+        #############################################################################
+        _out_block = astnodes.Block(label=None, body=[_function_wrapper])
+        _out_region = astnodes.Region(body=[_out_block])
+        _module = astnodes.Module(name=None, attributes=None, region=_out_region, location=None)
+        _mlirfile = astnodes.MLIRFile(definitions=[], modules=[_module])
+        #############################################################################
+
+        return _module
