aa5317acbf2a1f764c1d6bd2d8c5fbcec04dcf1c
CodeS1gn <codefisheng@gmail.com>
2021-09-26 09:32:36 +0000

    refactor return_op pass to stmt node mapping pass, that handles conversion rule of all python stmt

--- cancer_frontend/pass_manager/passes/statement_conversion_pass.py
+++ cancer_frontend/pass_manager/passes/statement_conversion_pass.py
@@ -10,6 +10,60 @@ __all__ = [
 
 
 class StatementConversionPass(PassBase):
+    """
+    this pass converts all python statements into relevant mlir astnodes
+    will find all statements nodes, and set its mast_node value,
+
+    the last step of this pass will be the check pass that checks all nodes
+    that belongs to statements.
+
+    should impl is_stmt and is_conversion_ready methods
+
+    follow EBNF rules as:
+
+    stmt = FunctionDef(identifier name, arguments args,
+                       stmt* body, expr* decorator_list, expr? returns,
+                       string? type_comment)
+          | AsyncFunctionDef(identifier name, arguments args,
+                             stmt* body, expr* decorator_list, expr? returns,
+                             string? type_comment)
+
+          | ClassDef(identifier name,
+             expr* bases,
+             keyword* keywords,
+             stmt* body,
+             expr* decorator_list)
+          | Return(expr? value)
+
+          | Delete(expr* targets)
+          | Assign(expr* targets, expr value, string? type_comment)
+          | AugAssign(expr target, operator op, expr value)
+          -- 'simple' indicates that we annotate simple name without parens
+          | AnnAssign(expr target, expr annotation, expr? value, int simple)
+
+          -- use 'orelse' because else is a keyword in target languages
+          | For(expr target, expr iter, stmt* body, stmt* orelse, string? type_comment)
+          | AsyncFor(expr target, expr iter, stmt* body, stmt* orelse, string? type_comment)
+          | While(expr test, stmt* body, stmt* orelse)
+          | If(expr test, stmt* body, stmt* orelse)
+          | With(withitem* items, stmt* body, string? type_comment)
+          | AsyncWith(withitem* items, stmt* body, string? type_comment)
+
+          | Raise(expr? exc, expr? cause)
+          | Try(stmt* body, excepthandler* handlers, stmt* orelse, stmt* finalbody)
+          | Assert(expr test, expr? msg)
+
+          | Import(alias* names)
+          | ImportFrom(identifier? module, alias* names, int? level)
+
+          | Global(identifier* names)
+          | Nonlocal(identifier* names)
+          | Expr(expr value)
+          | Pass | Break | Continue
+
+          -- col_offset is the byte offset in the utf8 string the parser uses
+          attributes (int lineno, int col_offset, int? end_lineno, int? end_col_offset)
+    """
 
     __slots__ = [
         "solvers",
@@ -19,14 +73,11 @@ class StatementConversionPass(PassBase):
         # type: (None) -> None
         super().__init__()
         self.solvers = []
-        self.solvers.append(ReturnOpTransformer)
+        self.solvers.append(StmtNodeMappingTransformer)
 
     def run_pass(self, ast_root: ast.AST) -> ast.AST:
         for _solver in self.solvers:
             ast_root = _solver().visit(ast_root)
-            print(ast_root.body[0].body[0].mast_node.pretty())
-            print(ast_root.body[0].mast_node.pretty())
-            print(ast_root.mast_node.pretty())
             ast.fix_missing_locations(ast_root)
 
         return ast_root
--- cancer_frontend/pass_manager/transformers/__init__.py
+++ cancer_frontend/pass_manager/transformers/__init__.py
@@ -1,2 +1,2 @@
 from .identical_past_transformer import *
-from .return_op_transformer import *
+from .stmt_node_mapping_transformer import *
