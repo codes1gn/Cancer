efd59a0b9b73ed70fc513cd6db8433b41ea833b4
CodeS1gn <codefisheng@gmail.com>
2021-09-26 10:54:55 +0000

    refactor: MlirNode.pretty method moved to NodeVisitor and NodeTransformer

--- cancer_frontend/pass_manager/transformers/node_visitor_base.py
+++ cancer_frontend/pass_manager/transformers/node_visitor_base.py
@@ -1,6 +1,53 @@
 import ast
+from mlir import astnodes
+
+MlirNode = astnodes.Node
 
 
 class NodeVisitorBase(ast.NodeVisitor):
     def __init__(self):
         super().__init__()
+
+    def pretty_mlir(self, node: MlirNode) -> str:
+        result = node.dump_ast()
+        lines = [""]
+        indent = 0
+        for index in range(len(result)):
+            char = result[index]
+            indent_word = "  "
+
+            if char == " ":
+                continue
+
+            if char == "[" and result[index + 1] == "]":
+                indent += 1
+                lines[-1] += char
+                continue
+
+            if char == ",":
+                lines[-1] += char
+                lines.append(indent * indent_word)
+                continue
+
+            if char == "[":
+                indent += 1
+                lines[-1] += char
+                lines.append(indent * indent_word)
+                continue
+            if char == "]":
+                indent -= 1
+
+            if char == "(":
+                indent += 1
+                lines[-1] += char
+                lines.append(indent * "  ")
+                continue
+            if char == ")":
+                indent -= 1
+
+            if char != "\n":
+                lines[-1] += char
+            if char == "\n":
+                lines.append(indent * indent_word)
+
+        return "\n".join(lines)
