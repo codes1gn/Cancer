ec103a1f436d9ca525c913450cedddb9d391860d
CodeS1gn <codefisheng@gmail.com>
2021-10-09 05:42:47 +0000

    hotfix: specify clang-11 as default compiler

--- scripts/build_and_install.sh
+++ scripts/build_and_install.sh
@@ -16,6 +16,8 @@ cd ${top_dir_realpath}/build
 cmake -G Ninja .. \
     -DMLIR_DIR=${top_dir_realpath}/mlir_build/install_dir/lib/cmake/mlir \
     -DLLVM_EXTERNAL_LIT=${top_dir_realpath}/mlir_build/bin/llvm-lit \
+    -DCMAKE_C_COMPILER=clang-11 \
+    -DCMAKE_CXX_COMPILER=clang++-11 \
     -DCMAKE_BUILD_TYPE=DEBUG \
     -DLLVM_USE_LINKER=lld
 
