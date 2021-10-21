029736cb4e6c2ee2ff2980576ed367e855af2c20
姜昆 <kun.jiang@enflame-tech.com>
2021-10-21 15:02:09 +0800

    refactor: Add pass_manager'scode comments.

--- cancer_frontend/pass_manager/pass_registry.py
+++ cancer_frontend/pass_manager/pass_registry.py
@@ -1,19 +1,22 @@
-from cancer_frontend.scaffold.utils import *
-
-from .passes import *
+from cancer_frontend.scaffold.utils import memoized_classproperty
+from .passes import IdenticalPastPass, StatementConversionPass
 
 
 # lazy load the pass register
 class PassRegistry(object):
     @memoized_classproperty
-    def pass_table(cls):
+    def pass_table(cls) -> dict:
+        """register passes into registry, will only run once
 
-        # register passes into registry
-        # will only run once
+        Returns:
+            dict: pass table that pass name is key and pass str(id) is value
+        """
+
         # TODO(albert) add more passes
         _pass_table = {}
 
         def register_pass(pass_class):
+            nonlocal _pass_table
             _pass_table[pass_class.__name__] = str(register_pass.id_cnt)
             register_pass.id_cnt += 1
 
@@ -21,5 +24,5 @@ class PassRegistry(object):
         register_pass.id_cnt = 0
         register_pass(IdenticalPastPass)
         register_pass(StatementConversionPass)
-
+        print("_pass_table == > ", _pass_table)
         return _pass_table
--- cancer_frontend/pass_manager/past_to_mlir_pass_manager.py
+++ cancer_frontend/pass_manager/past_to_mlir_pass_manager.py
@@ -1,5 +1,5 @@
 from cancer_frontend.pass_manager.pass_manager_base import PassManagerBase
-from cancer_frontend.pass_manager.passes import *
+from cancer_frontend.pass_manager.passes import IdenticalPastPass, StatementConversionPass
 
 __all__ = [
     "PastToMlirPassManager",
@@ -7,8 +7,14 @@ __all__ = [
 
 
 class PastToMlirPassManager(PassManagerBase):
+    """The class inherit PassManagerBase that to register passes.
+
+    Attributions:
+        None.
+    """
     def register_passes(self):
-        print("PastToMlirPassManager::register_passes")
+        """Register passes via add_pass func in PassManagerBase.
+        """
         self.add_pass(IdenticalPastPass)
         self.add_pass(StatementConversionPass)
 
