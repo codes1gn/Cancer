b303c207d72f73cc13fe23040b9ac1c87bf93a8f
CodeS1gn <codefisheng@gmail.com>
2021-10-25 11:00:29 +0000

    fix passes registration impl and passes detail impl in lib/IREE/passes

--- cancer-compiler-src/lib/Backends/IREE/LowerLinkage.cpp
+++ cancer-compiler-src/lib/Backends/IREE/LowerLinkage.cpp
@@ -1,10 +1,3 @@
-//===----------------------------------------------------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
 
 #include "PassDetail.h"
 #include "Backends/IREE/Passes.h"
@@ -16,10 +9,20 @@ using namespace mlir::CANCER;
 using namespace mlir::CANCER::IREEBackend;
 
 namespace {
-#define GEN_PASS_REGISTRATION
-#include "Backends/IREE/Passes.h.inc"
-} // end namespace
+// This pass lowers the public ABI of the module to the primitives exposed by
+// the refbackrt dialect.
+class LowerLinkagePass : public LowerLinkageBase<LowerLinkagePass> {
+  void runOnOperation() override {
+    ModuleOp module = getOperation();
+    for (auto func : module.getOps<FuncOp>()) {
+      if (func.getVisibility() == SymbolTable::Visibility::Public)
+        func->setAttr("iree.module.export", UnitAttr::get(&getContext()));
+    }
+  }
+};
+} // namespace
 
-void mlir::CANCER::IREEBackend::registerIREEBackendPasses() {
-  ::registerPasses();
+std::unique_ptr<OperationPass<ModuleOp>>
+mlir::CANCER::IREEBackend::createLowerLinkagePass() {
+  return std::make_unique<LowerLinkagePass>();
 }
--- cancer-compiler-src/lib/Backends/IREE/Passes.cpp
+++ cancer-compiler-src/lib/Backends/IREE/Passes.cpp
@@ -1,3 +1,10 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
 #include "PassDetail.h"
 #include "Backends/IREE/Passes.h"
@@ -9,20 +16,10 @@ using namespace mlir::CANCER;
 using namespace mlir::CANCER::IREEBackend;
 
 namespace {
-// This pass lowers the public ABI of the module to the primitives exposed by
-// the refbackrt dialect.
-class LowerLinkagePass : public LowerLinkageBase<LowerLinkagePass> {
-  void runOnOperation() override {
-    ModuleOp module = getOperation();
-    for (auto func : module.getOps<FuncOp>()) {
-      if (func.getVisibility() == SymbolTable::Visibility::Public)
-        func->setAttr("iree.module.export", UnitAttr::get(&getContext()));
-    }
-  }
-};
-} // namespace
+#define GEN_PASS_REGISTRATION
+#include "Backends/IREE/Passes.h.inc"
+} // end namespace
 
-std::unique_ptr<OperationPass<ModuleOp>>
-mlir::CANCER::IREEBackend::createLowerLinkagePass() {
-  return std::make_unique<LowerLinkagePass>();
+void mlir::CANCER::IREEBackend::registerIREEBackendPasses() {
+  ::registerPasses();
 }
